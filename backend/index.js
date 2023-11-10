50a989c7d9d0a107558beff3913ba5a640366a2c
Amelie Cervall <amelie@grebban.com>
2023-11-10 18:47:45 +0100

    code cleanup console .log

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-const dotenv = require('dotenv');
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const dotenv = require("dotenv");
 
 dotenv.config();
 
-const userRouter = require('./routes/user');
+const userRouter = require("./routes/user");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -13,22 +13,30 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 app.use(
-  cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
-  })
+    cors({
+        origin: "*",
+        methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
+    })
 );
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/user', userRouter);
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/user", userRouter);
 
 app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  console.error(err.message, err.stack);
-  res.status(statusCode).json({ message: err.message });
-  return;
+    const statusCode = err.statusCode || 500;
+    console.error(err.message, err.stack);
+    res.status(statusCode).json({ message: err.message });
+    return;
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(port, async () => {
+    console.log(`Server is running on http://localhost:${port}`);
+
+    try {
+        const { rows } = await client.query("SELECT * FROM users");
+
+        console.log("All users:", rows);
+    } catch (error) {
+        console.error("Error fetching all users:", error);
+    }
 });
