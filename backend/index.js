885527f5fdf0cd38d7a517feb3805e2235d4a239
Amelie Cervall <amelie@grebban.com>
2023-11-10 18:52:36 +0100

    fix client

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const dotenv = require("dotenv");
+const client = require("../connection");
 
 dotenv.config();
 
